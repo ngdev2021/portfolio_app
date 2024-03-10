5119eba9954992115e62444293b22496d12dac48
ngdev2021 <90414221+ngdev2021@users.noreply.github.com>
2024-03-10 14:31:51 -0400

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Your Portfolio Application
+      </header>
+    </div>
+  );
+}
+
+export default App;
